f36775fe6989a9c8f2173de3df28bfb8b4632eb2
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-07 19:22:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,11 +7,3 @@ def square_array(array)
  square_array
 end
 
-def square_array(array)
-  square_array = []
- array.each do |sqnum|
-   num = sqnum * sqnum
-   square_array << num
- end
- square_array
-end
